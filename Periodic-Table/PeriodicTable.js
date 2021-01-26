9760ebc6b4cb6df76f35a3b9c55315a96f11d62b
myouderian22 <myouderian22@nscds.org>
2021-01-26 21:13:55 +0000

    Sizing adjustments + array changes

--- Periodic-Table/PeriodicTable.js
+++ Periodic-Table/PeriodicTable.js
@@ -1,17 +1,56 @@
 var tableArr1 = 
     [
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
-    [1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        [ 
+            {element:"Hydrogen"},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:"Helium"}
+        ],
+
+        [
+            {element:"Lithium"},
+            {element:"Beryllium"},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:"Boron"},
+            {element:"Carbon"},
+            {element:"Nitrogen"},
+            {element:"Oxygen"},
+            {element:"Fluorine"},
+            {element:"Neon"}
+        ],
+ //   [1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+ //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+ //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+ //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+ //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
 
 for(var c = 0; c < 7; c++){
     for(var r = 0; r < 18; r++){
-        if(tableArr1[c][r] == 1){
+        if(tableArr1[c][r].element !== -1){
             $('#container').append("<div class = 'element'></div>")
         }
         else{
