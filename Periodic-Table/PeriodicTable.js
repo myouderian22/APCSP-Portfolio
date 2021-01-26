4e045372be276f06a38c5e669a1f4f156dc17f1a
myouderian22 <myouderian22@nscds.org>
2021-01-26 21:22:43 +0000

    Adding to array

--- Periodic-Table/PeriodicTable.js
+++ Periodic-Table/PeriodicTable.js
@@ -41,20 +41,40 @@ var tableArr1 =
             {element:"Fluorine"},
             {element:"Neon"}
         ],
- //   [1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
- //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
- //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
- //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
- //   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+
+        [
+            {element:"Sodium"},
+            {element:"Magnesium"},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:-1},
+            {element:"Aluminium"},
+            {element:"Silicon"},
+            {element:"Phosphorus"},
+            {element:"Sulfur"},
+            {element:"Chlorine"},
+            {element:"Argon"}
+        ],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
 
 for(var c = 0; c < 7; c++){
     for(var r = 0; r < 18; r++){
         if(tableArr1[c][r].element !== -1){
-            $('#container').append("<div class = 'element'></div>")
+            $('#container').append("<div class = 'element'></div>");
         }
         else{
-            $('#container').append("<div class = 'empty'></div>")
+            $('#container').append("<div class = 'empty'></div>");
         }
     }
 }
